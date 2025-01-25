9fc5106400959ec0cf9845e5706b8b197cfe8616
C <44597237+cshowspeed@users.noreply.github.com>
2025-01-26 00:04:09 +0800

    pomodoro

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Container from "./components/Container";
+import "./App.css"
+
+export default function App() {
+    document.title = "Pomofocus Clone"
+    return (
+        <Container />
+    )
+}
